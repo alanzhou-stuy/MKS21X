18b359b121af877c09f2cedb24edddaf38bfcff9
Alan Zhou <azhou2@stuy.edu>
2016-12-05 22:58:06 -0500

    done

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -25,10 +25,31 @@ public class SlidingPuzzle {
 	return true;
     }
 
-    public boolean isDone() {
-
-    }
-
+    public boolean isDone2(){
+		int previous = values[0][0];
+		for(int r = 0; r < side; r++){
+			for(int c = 0; c < side; c++){
+				if(values[r][c] != 0 && values[r][c] == previous+1){
+					previous = values[r][c];
+				} else if(values[r][c] != 0 && values[r][c] != previous){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean isDone3(){
+		int previous = values[0][0];
+		for(int i = 0; i < side * side; i++){
+			if(values[i/side][i%side] != 0 && values[i/side][i%side] == previous+1){
+				previous = values[i/side][i%side];
+			} else if (values[i/side][i%side] != 0 && values[i/side][i%side] != previous){
+				return false;
+			}
+		}
+		return true;
+	}
     public void initialize() {
 	ArrayList<Integer> temp = new ArrayList<Integer>();
 	for (int j = 0; j< side*side; j++) 
