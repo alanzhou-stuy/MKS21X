79cc9f79174ea44258875c838a7388133ad162a7
Alan Zhou <azhou2@stuy.edu>
2016-12-07 22:23:30 -0500

    done

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -94,14 +94,4 @@ public class Barcode implements Comparable<Barcode>{
   
 
 
-public static void main(String args[]){
-	Barcode b = new Barcode("08451");
-	Barcode c = new Barcode("99999");
-	Barcode d = new Barcode("11111");
-	System.out.println(b); //084518 |||:::|::|::|::|:|:|::::|||::|:|
-	System.out.println(b.toString().compareTo("084518 |||:::|::|::|::|:|:|::::|||::|:|")); //0
-	System.out.println(b.compareTo(b)); //0
-	System.out.println(c.compareTo(b));
-	System.out.println(d.compareTo(b));
-}
 }
