fd8de90dafede3ed4deb0c4de04a4c8f39764f3e
Alan Zhou <azhou2@stuy.edu>
2016-12-13 05:54:02 -0500

    notdone

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -8,26 +8,26 @@ public class Sorts{
       int smallestNum = data[0];
       int temp = 0;
       for (int i = 0; i <  data.length - 2; i ++) {
-	  for (int n = 0; n < data.length; n++) {
-	      if (data[n] < smallestNum) {
-		  smallestNum = data[n];
+	  for (int n = 0; n < data.length - i; n++) {
+	      if (data[n + i] < smallestNum) {
+		  smallestNum = data[n + i];
 		  temp = n;
 	      }
 	  }
-	  if (smallestNum < data[i]) {
-		  data[temp] = data[i];
-		  data[i] = smallestNum; 
-	      }
-	  
+	  data[temp] = data[i];
+	  data[i] = smallestNum;   
       }
   }
 
     public static void main(String[] args) {
 	int[]data = {10, 9, 2, 3, 0, 11};
+	selectionSort(data);
 	String x = "{";
 	for (int i = 0; i < data.length; i++){
 	    x += data[i] + " ";
 	}
+	
+	System.out.println(x + "}");
     }
 }
 
