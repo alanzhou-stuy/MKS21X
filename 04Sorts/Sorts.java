f647306a36abc59e16a96e1773859b30193a538a
Alan Zhou <azhou2@stuy.edu>
2016-12-14 05:48:54 -0500

    done

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -4,6 +4,7 @@ public class Sorts{
       return "09.Zhou.Alan";
   }
 
+    //max work, always N
   public static void selectionSort(int[] data){  
       int smallestNum = data[0];
       int temp = data[0];
@@ -21,9 +22,28 @@ public class Sorts{
       }
   }
 
+    //has potential for les work, average list is n/2 insertions  
+    public static void insertionSort(int[] data) {
+	for(int t = 1; t < data.length; t++) {
+	    int num = data[t];
+	    int temp = 0;
+	    if (data[t - 1] > num) {
+		for (int i = t - 1; i > -1; i--) {
+		    if (data[i] > num) {
+			temp = data[i];
+			data[i] = num;
+			data[i + 1] = temp;
+		    }
+		}
+	    }
+	}
+    }
+
+
     public static void main(String[] args) {
 	int[]data = {10, 9, 2, 3, 0, 11, 0, 0, 0, 3};
-	selectionSort(data);
+	//selectionSort(data);
+	insertionSort(data);
 	String x = "{";
 	for (int i = 0; i < data.length; i++){
 	    x += data[i] + " ";
